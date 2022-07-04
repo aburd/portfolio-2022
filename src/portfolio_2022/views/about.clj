62033f8d095d1da0c6667af8f724199c9565b53e
aburd <burdick.aaron@gmail.com>
2022-07-04 20:16:06 +0900

    fix style

--- src/portfolio_2022/views/about.clj
+++ src/portfolio_2022/views/about.clj
@@ -6,5 +6,14 @@
         locale (:locale req)]
     [:div {:class "page-about"} 
      [:h3 (t locale :about-website-title)]
-     [:p {:class "page-body-text"} (t locale :about-website)]]))
+     [:p {:class "page-body-text"} (t locale :about-website)]
+     [:iframe
+      {:allowfullscreen "allowfullscreen",
+       :allow
+       "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+       :frameborder "0",
+       :title "YouTube video player",
+       :src "https://www.youtube.com/embed/SxdOUGdseq4",
+       :height "315",
+       :width "560"}]]))
     
