0393c81000f48060c3e5898792515a6995c2ddaa
aburd <burdick.aaron@gmail.com>
2022-07-06 18:50:10 +0900

    add more information to job experience

--- src/portfolio_2022/views/core.clj
+++ src/portfolio_2022/views/core.clj
@@ -11,26 +11,34 @@
          [:body inner-html]
          [:script {:src "/js/main.js"}]]))
 
+(defn nav-option-active 
+  "Check if the nav-options is active based on request data."
+  [option req]
+  (= (:url option) (-> req :reitit.core/match :path)))
+  
+
 (defn nav 
-  "Returns a nav hiccup navbar with some list of options.
-  The `active` arg will add an 'active' class."
-  [options active]
+  "Returns a nav hiccup navbar with some list of options."
+  [options]
   [:nav {:class "main-nav"}
    [:button {:class "start-btn"} "Start"]
    [:ul
     (for [option options]
-      [:li
-       [:a {:href (:url option)} (s/capitalize (:text option))]])]])
+      (let [class-name (if (:active option) "active" "")]
+        [:li {:class class-name}
+         (if (:active option)
+           (s/capitalize (:text option))
+           [:a {:href (:url option)} (s/capitalize (:text option))])]))]])
 
 (defn switch
-  "Makes a toggle switch"
-  [on name]
+  "Makes a toggle switch."
+  [on switch-name]
   [:label {:class "switch"}
-   [:input {:type "checkbox" :checked on :name name}]
+   [:input {:type "checkbox" :checked on :name switch-name}]
    [:span {:class "slider round"}]])
 
 (defn locale-form 
-  "A form which will allow the user to change languages"
+  "A form which will allow the user to change languages."
   [req]
   (let [{locale :locale uri :uri query-string :query-string} req
         locales [{:value :en :display "English"}
@@ -45,7 +53,7 @@
         [:option {:value value :selected (= value locale)} display])]]))
 
 (defn social
-  "Returns social icons"
+  "Returns social icons."
   [req]
   [:div {:class "social-icons"}
    [:ul
@@ -64,13 +72,16 @@
   [html-fn req]
   (let [t (:t req)
         locale (:locale req)
-        nav-options [{:url (str "/") :text (t locale :ui/nav/home)}
-                     {:url (str "/" "about") :text (t locale :ui/nav/about)}
-                     {:url (str "/" "works") :text (t locale :ui/nav/works)}
-                     {:url (str "/" "terminal") :text (t locale :ui/nav/terminal)}
-                     {:url (str "/" "contact") :text (t locale :ui/nav/contact)}]]
+        nav-options (map 
+                      #(merge % {:active (nav-option-active % req)}) 
+                      [{:url (str "/") :text (t locale :ui/nav/home)}
+                       {:url (str "/" "about") :text (t locale :ui/nav/about)}
+                       {:url (str "/" "works") :text (t locale :ui/nav/works)}
+                       {:url (str "/" "terminal") :text (t locale :ui/nav/terminal)}
+                       {:url (str "/" "contact") :text (t locale :ui/nav/contact)}])]
+        
     (html-view-wrap [:div {:class "container"}
-                     [:header [:div {:class "container-nav"} (nav nav-options nil)]
+                     [:header [:div {:class "container-nav"} (nav nav-options)]
                               [:div {:class "container-social"} (social req)]]
                      [:main (html-fn req)]
                      [:footer]])))
